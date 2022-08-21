a02bc25deaa20ce7837acc819f8b4e31f25af1f6
linran <linranff@gmail.com>
2022-08-21 16:47:25 +0800

    Update Collatz.java
    lab1 Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,21 +1,29 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author linf
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /**
+     * Buggy implementation of nextNumber!
+     *
+     * @return
+     */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
+        /** If n is even, the next number is n/2 */
+        if (n % 2 == 0) {
+            return n/2;
+        /** If n is odd, the next number is 3n + 1 */
+        } else if (n % 2 == 1)  {
             return 3 * n + 1;
         } else {
-            return n * 2;
+            return 0;
         }
+
+
     }
 
     public static void main(String[] args) {
-        int n = 5;
+        int n = 1234;
         System.out.print(n + " ");
         while (n != 1) {
             n = nextNumber(n);
