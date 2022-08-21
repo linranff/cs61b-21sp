30d4eabb8d7816dffbc5350250a4788464ace936
linran <linranff@gmail.com>
2022-08-21 16:52:34 +0800

    Update Collatz.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -23,7 +23,7 @@ public class Collatz {
     }
 
     public static void main(String[] args) {
-        int n = 1234;
+        int n = 5;
         System.out.print(n + " ");
         while (n != 1) {
             n = nextNumber(n);
